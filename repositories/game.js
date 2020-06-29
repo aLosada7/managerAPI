12f581f8e0ba2eed229c2f4e495d59f9f6cac000
Alvaro <aldc30sc@gmail.com>
2020-06-29 11:05:22 +0200

    changes

--- repositories/game.js
+++ repositories/game.js
@@ -9,16 +9,25 @@ class GameRepository {
      * Simulate game
      */
     static async simulateGame(gameId) {
-        let game = await Game.findById(gameId);
+        let game = await Game.findById(gameId)
+        .populate({
+            path: 'homeTeam',
+            select: 'name arenaName arenaSize'
+        }).populate({
+            path: 'visitorTeam',
+            select: 'name'
+        });
 
-        /*if(game.winner) {
+        if(game.winner) {
             return game;
-        }*/
-    
+        }
+
         const local = await PlayerRepository.findTeamPlayers(game.homeTeam);
         const visitor = await PlayerRepository.findTeamPlayers(game.visitorTeam);
 
         const simulatedGame = await SimulateGameService.simulateGame(game, local, visitor);
+
+        //simulatedGame['homeTeam'] = game.homeTeam;
 
         game = await Game.findByIdAndUpdate(gameId, simulatedGame, {
             new: true,
